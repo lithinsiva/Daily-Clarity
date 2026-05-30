159d04a082116e07dc8636c146fb910e318033d5
lithinsiva <lithinsiva700@gmail.com>
2026-05-30 18:51:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This tells the browser to find assets relative to index.html
 })
